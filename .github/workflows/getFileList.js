5cdf57148b1e762126e807c472ba02081dc74b20
waynebruce0x <waynebruce0x@gmail.com>
2023-03-16 15:19:07 +0000

    arrrr

--- .github/workflows/getFileList.js
+++ .github/workflows/getFileList.js
@@ -4,16 +4,16 @@ const MODIFIED = parse(process.env.MODIFIED);
 const ADDED = parse(process.env.ADDED);
 const fileSet = new Set();
 
-[...MODIFIED, ...ADDED].forEach(file => {
-  const [root, dir] = file.split("/");
-  if (root === "protocols") fileSet.add(root + "/" + dir);
-});
+// [...MODIFIED, ...ADDED].forEach(file => {
+//   const [root, dir] = file.split("/");
+//   if (root === "protocols") fileSet.add(root + "/" + dir);
+// });
 
-console.log(JSON.stringify([...fileSet]));
+// console.log(JSON.stringify([...fileSet]));
 
-function parse(data) {
-  console.log("data below:");
-  console.log(data);
-  console.log("data above:");
-  return data; //.replace("[", "").replace("]", "").split(",");
-}
+// function parse(data) {
+//   console.log("data below:");
+//   console.log(data);
+//   console.log("data above:");
+//   return data; //.replace("[", "").replace("]", "").split(",");
+// }
