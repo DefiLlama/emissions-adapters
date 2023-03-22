54953c7f8243c649361e42af0aba77b6b1ed8bf3
define <Lovelovenow@protonmail.com>
2023-03-22 17:29:35 +0000

    acala and compound (wip)

---
+++ protocols/acala.ts
@@ -0,0 +1,25 @@
+import { manualCliff, manualLinear } from "../adapters/manual";
+import { Protocol } from "../types/adapters";
+import { periodToSeconds } from "../utils/time";
+
+const start = 1643112000;
+const qty = 1_000_000_000;
+const start_Investors = 1675252800;
+const start_Team = 1675252800;
+const start_Treasury = 1643112000;
+
+const acala: Protocol = {
+  Ecosystem: manualCliff(start, qty * 0.05),
+  Treasury: manualCliff(start_Treasury, qty * 0.1162),
+  Team: manualLinear(start_Team, start_Team + periodToSeconds.week * 17, qty * 0.2525),
+  Rewards: manualLinear(start, start + periodToSeconds.week * 72, qty * 0.34),
+  Investors: manualLinear(start_Investors, start_Investors + periodToSeconds.week * 18, qty * 0.2913),
+  notes: [
+    `Investors vesting is between 12-24 months , so we will use 18 months`,
+  ],
+  token: "coingecko:acala",
+  sources: ["https://github.com/AcalaNetwork/Acala-white-paper/blob/master/Acala_Token_Economy_Paper.pdf"],
+  protocolIds: null,
+};
+
+export default acala;
---
+++ protocols/compound.ts
@@ -0,0 +1,23 @@
+import { manualCliff, manualLinear } from "../adapters/manual";
+import { Protocol } from "../types/adapters";
+import { periodToSeconds } from "../utils/time";
+
+const start = 1592222400;
+const qty = 1_000_000_000;
+
+
+const compound: Protocol = {
+  Liquidity_Mining: manualCliff(start, qty * 0.421),
+  Team_Founders: manualLinear(start, start + periodToSeconds.week * 208, qty * 0.225),
+  ShareHolders: manualCliff(start, qty * 0.24),
+  Community: manualCliff(start, qty * 0.077),
+  Future_Team: manualCliff(start, qty * 0.037),
+  notes: [
+    `We were unable to find details about the team's unlock schedule. Therefore we've extrapolated their vesting rate (as of 14 Mar '23) to estimate an average Team unlock schedule.`,
+  ],
+  token: "ethereum:0xc00e94cb662c3520282e6f5717214004a7f26888",
+  sources: ["https://medium.com/compound-finance/compound-governance-decentralized-b18659f811e0"],
+  protocolIds: ["114"],
+};
+
+export default compound;
