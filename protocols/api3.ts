550959d31f1cd5e9d554e0dfd475e95402203f94
umersin61 <86656427+umersin61@users.noreply.github.com>
2023-04-01 18:30:49 +0200

    add api3 vesting info

---
+++ protocols/api3.ts
@@ -0,0 +1,17 @@
+import { manualCliff, manualLinear } from "../adapters/manual";
+import { Protocol } from "../types/adapters";
+import { periodToSeconds } from "../utils/time";
+
+const start = 1606741200;
+const qty = 100_000_000_000;
+
+const api3: Protocol = {
+  Team: manualLinear(start, start + periodToSeconds.year * 3, qty * 0.3),
+  Contributors: manualLinear(start, start + periodToSeconds.year * 3, qty * 0.10),
+  Seed: manualLinear(start, start + periodToSeconds.year * 2, qty * 0.15),
+  token: "coingecko:api3",
+  sources: ["https://medium.com/api3/api3-public-token-distribution-event-1acb3b6d940"],
+  protocolIds: [],
+};
+
+export default api3;
