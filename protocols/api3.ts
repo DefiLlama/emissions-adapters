871e6f3851df84787b0323fd6d5fb3167f19f7ba
umersin61 <86656427+umersin61@users.noreply.github.com>
2023-04-02 17:34:35 +0200

    adjusted some wording

--- protocols/api3.ts
+++ protocols/api3.ts
@@ -6,12 +6,12 @@ const start = 1606741200;
 const qty = 100_000_000_000;
 
 const api3: Protocol = {
-  Team: manualLinear(start, start + periodToSeconds.year * 3, qty * 0.3),
-  Contributors: manualLinear(start, start + periodToSeconds.year * 3, qty * 0.1),
-  PreSeed: manualLinear(start, start + periodToSeconds.year * 2, qty * 0.05),
-  Seed: manualLinear(start, start + periodToSeconds.year * 2, qty * 0.1),
-  Treasury: manualCliff(start, qty * 0.25),
-  PublicDistribution: manualCliff(start, qty * 0.2),
+  "Founding Team": manualLinear(start, start + periodToSeconds.year * 3, qty * 0.3),
+  "Partners & Contributors": manualLinear(start, start + periodToSeconds.year * 3, qty * 0.1),
+  "Pre-seed Investors": manualLinear(start, start + periodToSeconds.year * 2, qty * 0.05),
+  "Seed Investors": manualLinear(start, start + periodToSeconds.year * 2, qty * 0.1),
+  "Ecosystem Fund": manualCliff(start, qty * 0.25),
+  "Public distribution": manualCliff(start, qty * 0.2),
   token: "coingecko:api3",
   sources: ["https://medium.com/api3/api3-public-token-distribution-event-1acb3b6d940"],
   protocolIds: [],
