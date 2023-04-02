57aec7476c4511e16d4dc9abd1126215675c7183
umersin61 <86656427+umersin61@users.noreply.github.com>
2023-04-02 17:30:26 +0200

    add treasury + public distribution

--- protocols/api3.ts
+++ protocols/api3.ts
@@ -9,6 +9,8 @@ const api3: Protocol = {
   Team: manualLinear(start, start + periodToSeconds.year * 3, qty * 0.3),
   Contributors: manualLinear(start, start + periodToSeconds.year * 3, qty * 0.10),
   Seed: manualLinear(start, start + periodToSeconds.year * 2, qty * 0.15),
+  Treasury: manualCliff(start, qty * 0.25),
+  PublicDistribution: manualCliff(start, qty * 0.2),
   token: "coingecko:api3",
   sources: ["https://medium.com/api3/api3-public-token-distribution-event-1acb3b6d940"],
   protocolIds: [],
