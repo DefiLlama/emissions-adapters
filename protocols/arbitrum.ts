12b77dc10434c3db21d83ffc097b6d30a4c254fc
define <Lovelovenow@protonmail.com>
2023-04-03 12:44:00 +0100

    Arbitrum Tokenomics

---
+++ protocols/arbitrum.ts
@@ -0,0 +1,43 @@
+import { manualCliff, manualLinear, manualLog } from "../adapters/manual";
+import { Protocol } from "../types/adapters";
+import { periodToSeconds } from "../utils/time";
+
+const start = 1679533271;
+const qty = 10_000_000_000;
+const qty_advisors = 2_694_000_000;
+const qty_investors = 1_753_000_000;
+const end_team_investors_1year = 1711155671
+
+
+
+const arbitrum: Protocol = {
+    "Arbitrum DAO Treasury": manualCliff(start, qty * 0.4278),
+    "Advisors Team OffchainLabs": [
+        manualCliff(start, qty * 0.25, "unix"), // 25% cliff after 1 year
+        manualLinear(
+          end_team_investors_1year,
+          end_team_investors_1year + periodToSeconds.month * 12 * 3, 
+          qty_advisors * 0.75 / 36, 
+          "unix"
+        ), // monthly steps for the next 3 years
+      ],
+    Investors: [
+        manualCliff(start, qty * 0.25, "unix"), // 25% cliff after 1 year
+        manualLinear(
+          end_team_investors_1year,
+          end_team_investors_1year + periodToSeconds.month * 12 * 3, 
+          qty_investors * 0.75 / 36, 
+          "unix"
+        ), // monthly steps for the next 3 years
+      ],
+    Airdrop: manualCliff(start, qty * 0.1162),
+    "Ecosystem Development Fund":manualCliff(start, qty * 0.0113),
+  notes: [
+    `No clear mention regarding if the team founders and investors have their tokens locked`,
+  ],
+  token: "arbitrum:0x912ce59144191c1204e64559fe8253a0e49e6548",
+  sources: ["https://docs.arbitrum.foundation/airdrop-eligibility-distribution#initial-token-allocation--airdrop-distribution"],
+  protocolIds: [null], // add listing
+};
+
+export default arbitrum;
