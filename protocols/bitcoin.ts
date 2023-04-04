d5d46a4281d391d2ae5cf133db5176fd5f8c2e46
waynebruce0x <waynebruce0x@gmail.com>
2023-04-04 13:46:59 +0100

    btc

---
+++ protocols/bitcoin.ts
@@ -0,0 +1,43 @@
+import { manualLinear } from "../adapters/manual";
+import { LinearAdapterResult, Protocol } from "../types/adapters";
+
+const initialReward: number = 50;
+const halveningDates: string[] = [
+  "2009/01/03", // 0
+  "2012/11/28", // 210k
+  "2016/07/09", // 420k
+  "2020/05/11", // 630k
+  "2024/04/28/", // 840k estimated
+  "2028/06/09", // 1050k estimated
+];
+
+function rewards(): LinearAdapterResult[] {
+  const sections: LinearAdapterResult[] = []
+  let reward: number = initialReward;
+
+  for (let i = 0; i < halveningDates.length - 1; i++) {
+    const qty = reward * 210_000
+    sections.push(
+        manualLinear(halveningDates[i], halveningDates[i+1], qty)
+    );
+    reward /= 2;
+  }
+
+  return sections
+}
+
+const bitcoin: Protocol = {
+  "Mining rewards": rewards(),
+  sources: [
+    "https://cryptoanswers.com/faq/bitcoin-halving-dates-history/",
+    "https://blockstream.info/",
+    "https://www.coingecko.com/en/coins/bitcoin/bitcoin-halving",
+  ],
+  token: "coingecko:bitcoin",
+  protocolIds: [],
+  notes: [
+    `Future halvenings have at a specified block height, therefore the dates shown are an estimate only.`,
+  ],
+};
+
+export default bitcoin;
