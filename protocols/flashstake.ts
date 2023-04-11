b5ddc90ab1d9727874b4cae0fc6f55a98bbc81dc
waynebruce0x <waynebruce0x@gmail.com>
2023-04-11 17:15:25 +0100

    flashstake

---
+++ protocols/flashstake.ts
@@ -0,0 +1,35 @@
+import { Protocol } from "../types/adapters";
+import { manualCliff, manualLinear } from "../adapters/manual";
+import { periodToSeconds } from "../utils/time";
+
+const qty = 150000000;
+const start = 1638316800;
+
+const flashstake: Protocol = {
+  "Community airdrop": manualCliff(start, qty * 0.1866),
+  "Team budget": [
+    manualCliff(start, qty * 0.0525),
+    manualLinear(start, start + periodToSeconds.year, qty * 0.075),
+    manualLinear(
+      start + periodToSeconds.year,
+      start + periodToSeconds.year * 2,
+      qty * 0.0225,
+    ),
+  ],
+  "Growth budget": manualCliff(start, qty * 0.3884),
+  "Staking budget": manualLinear(
+    start,
+    start + periodToSeconds.year,
+    qty * 0.1234,
+  ),
+  "Blockzero treasury": [
+    manualCliff(start, qty * 0.0303),
+    manualLinear(start, start + periodToSeconds.year, qty * 0.1214),
+  ],
+  sources: ["https://docs.flashstake.io/tokenomics-flash/token-distribution"],
+  notes: [],
+  token: "ethereum:0xb1f1f47061a7be15c69f378cb3f69423bd58f2f8",
+  protocolIds: ["2115"],
+};
+
+export default flashstake;
