bbebdd72ee9a8d0debebb2f636b122faa10b8a0f
define <Lovelovenow@protonmail.com>
2023-04-17 12:34:17 +0100

    Arbitrum Exchange emissions adapter

---
+++ protocols/arbitrum-exchange.ts
@@ -0,0 +1,22 @@
+import { manualCliff, manualLinear } from "../adapters/manual";
+import { Protocol } from "../types/adapters";
+import { periodToSeconds } from "../utils/time";
+
+const start = 1678975200;
+const qty = 20_000_000;
+
+
+const arbidex: Protocol = {
+"Initial Mint": manualCliff(start, qty * 0.029),
+"Treasury": manualLinear(start, start + periodToSeconds.year* 2, qty * 0.05),
+"Liquidity Incentives":manualLinear(start, start + periodToSeconds.month* 36, qty * 0.871),
+"Team": manualLinear(start, start + periodToSeconds.year * 2, qty * 0.05),
+  notes: [
+    'Initial Mint was composed by : Seed Investors (36.5%), Pre Sale (56.04%), Treasury (30%), Team (30%)'
+  ],
+  token: "arbitrum:0xD5954c3084a1cCd70B4dA011E67760B8e78aeE84",
+  sources: ["https://arbitrum-exchange.gitbook.io/arbitrumdex/usdarx-tokenomics/usdarx-token"],
+  protocolIds: ["2685"],
+};
+
+export default arbidex;
