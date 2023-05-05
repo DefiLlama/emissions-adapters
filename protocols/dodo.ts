3aa223c9d764653db738652592b6dea296cc3151
waynebruce0x <waynebruce0x@gmail.com>
2023-05-05 15:24:26 +0100

    dodo

--- protocols/dodo.ts
+++ protocols/dodo.ts
@@ -1,9 +1,13 @@
 import { Protocol } from "../types/adapters";
 import { manualCliff, manualLinear } from "../adapters/manual";
 import { periodToSeconds } from "../utils/time";
+import { daoSchedule, latestDao } from "../adapters/balance";
 
 const qty = 1000000000;
 const start = 1597449600;
+const token = "0x43dfc4159d86f3a37a5a4b3d4580b888ad7d4ddd";
+const chain = "ethereum";
+const timestampDeployed = 1602457200;
 
 const dodo: Protocol = {
   "Team & consultants": manualLinear(
@@ -26,14 +30,24 @@ const dodo: Protocol = {
   ],
   IDO: manualCliff(start, qty * 0.01),
   "Operations, marketing, partners": manualCliff(start, qty * 0.08),
-  //"Community incentives": manualCliff(start, qty * 0.6),
+  "Community incentives": daoSchedule(
+    qty * 0.6,
+    "0x4447183c50e82a8b0141718c405381a3b1bad634",
+    token,
+    chain,
+    "dodo",
+    timestampDeployed,
+  ),
   meta: {
     sources: ["https://docs.dodoex.io/english/tokenomics/dodo-allocation"],
-    token: "ethereum:0x43dfc4159d86f3a37a5a4b3d4580b888ad7d4ddd",
+    token: `${chain}:${token}`,
     notes: [
       `Community incentives (60%) are released on an uncofirmed schedule, so have been excluded from this analysis.`,
     ],
     protocolIds: ["146"],
+    custom: {
+      latestTimestamp: latestDao("dodo", timestampDeployed),
+    },
   },
   sections: {
     insiders: [
