5681bb44c8961cba04b85f742f66938ad7e28f5c
waynebruce0x <waynebruce0x@gmail.com>
2023-05-11 10:04:21 +0200

    getBlock from API

--- utils/block.ts
+++ utils/block.ts
@@ -1,8 +1,11 @@
+import fetch from "node-fetch";
 import { getBlock } from "@defillama/sdk/build/computeTVL/blocks";
+import { lookupBlock } from "@defillama/sdk/build/util";
 
 const secondsPerBlock = {
   ethereum: 12,
 };
+
 export async function estimateBlockHeight(timestamp: number, chain: any) {
   const current = await getBlock(chain);
   return (
@@ -18,4 +21,16 @@ export async function estimateBlockTimestamp(height: number, chain: any) {
     (current.number - height) *
       secondsPerBlock[chain as keyof typeof secondsPerBlock]
   );
+}
+
+export async function getBlock2(
+  chain: any,
+  timestamp: number,
+): Promise<number> {
+  if (["polygon_zkevm", "vision", "era"].includes(chain))
+    return lookupBlock(timestamp, { chain }).then(blockData => blockData.block);
+  const res = await (await fetch(
+    `https://coins.llama.fi/block/${chain}/${timestamp}`,
+  )).json();
+  return res.height;
 }
