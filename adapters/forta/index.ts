4edff32ba7e7340b2f14b0cd8cc06c1fa35a983b
0xngmi <0xngmi@protonmail.com>
2023-05-15 19:01:28 +0100

    yet another fix

--- adapters/forta/index.ts
+++ adapters/forta/index.ts
@@ -4,7 +4,7 @@ import contracts from "./contracts";
 export const unallocated = () =>
   Promise.all(
     Object.keys(contracts).map((k: any) =>
-      ()=>daoSchedule(
+      daoSchedule(
         391000000,
         contracts[k].owners,
         contracts[k].token,
