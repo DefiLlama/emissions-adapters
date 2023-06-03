b2c997fc6b2b3b1ce87e46ea1d49debb53d4b128
0xngmi <80541789+0xngmi@users.noreply.github.com>
2023-06-03 05:10:32 +0100

    fix order

--- protocols/jpegd.ts
+++ protocols/jpegd.ts
@@ -6,6 +6,8 @@ const start = 1646092800;
 const qty = 69420000000;
 
 const jpegd: Protocol = {
+  "donation event": manualCliff(start, qty * 0.3),
+  treasury: manualCliff(start, qty * 0.35),
   team: manualLinear(
     start + 6 * periodToSeconds.month,
     start + (6 * periodToSeconds.month + 2 * periodToSeconds.year),
@@ -16,8 +18,6 @@ const jpegd: Protocol = {
     start + (6 * periodToSeconds.month + 2 * periodToSeconds.year),
     qty * 0.05,
   ),
-  "donation event": manualCliff(start, qty * 0.3),
-  treasury: manualCliff(start, qty * 0.35),
   meta: {
     token: "ethereum:0xE80C0cd204D654CEbe8dd64A4857cAb6Be8345a3",
     sources: ["https://docs.jpegd.io/jpegd-dao/tokenomics/supply-graph"],
