5f6565bc3f2c8799b1e64f93684962134ccb4fcd
diego <admin@dbltecnologia.com.br>
2023-06-02 22:44:54 -0300

    Updated Emission Schedule for Equilibre Community and Other Entities

--- protocols/equilibre.ts
+++ protocols/equilibre.ts
@@ -1,6 +1,8 @@
 import { Protocol } from "../types/adapters";
 import { manualCliff, manualLinear } from "../adapters/manual";
 import { periodToSeconds } from '../utils/time';
+import incentives from "../adapters/curve/community";
+
 
 const initialSupply = 40000000;
 const weeklyDecayRate = 0.01;
@@ -17,14 +19,23 @@ const weeklyDecay = (supply: number, weeks: number): number => {
 function equilibre(): Protocol {
   const weeksSinceStart = (Date.now() / 1000 - startTimestamp) / oneWeekInSeconds;
 
+  const equilibreCommunity = async ()=>incentives(
+    "0xE1da44C0dA55B075aE8E2e4b6986AdC76Ac77d73",
+    startTimestamp,
+    initialSupply,
+    weeklyDecayRate,
+    periodToSeconds.week,
+  );
+
   return {
+    
     community: manualLinear(
       startTimestamp,
       startTimestamp + weeksSinceStart * oneWeekInSeconds,
       weeklyDecay(initialSupply, weeksSinceStart),
     ),
     team: [
-      manualCliff(startTimestamp, initialSupply * 0.15),
+      manualCliff(startTimestamp, initialSupply * 0.15), 
       manualLinear(
         startTimestamp + 0.5 * periodToSeconds.year,
         startTimestamp + periodToSeconds.year,
@@ -39,8 +50,9 @@ function equilibre(): Protocol {
     grants: manualCliff(startTimestamp, initialSupply * 0.4),
     airdrop: manualCliff(startTimestamp, initialSupply * 0.0125),
     liquidity: manualCliff(startTimestamp, initialSupply * 0.0025),
+    "continuing emissions": equilibreCommunity,
     meta: {
-      sources: ["https://equilibre-finance.gitbook.io/equilibre/emissions", "https://equilibre-finance.gitbook.io/equilibre/initial-distribution"],
+      sources: ["https://equilibre-finance.gitbook.io/equilibre-v1/protocol-overview/tokenomics/emissions", "https://equilibre-finance.gitbook.io/equilibre-v1/protocol-overview/tokenomics/initial-distribution"],
       token: "vara:0xE1da44C0dA55B075aE8E2e4b6986AdC76Ac77d73", 
       protocolIds: ["566"],  
     },
