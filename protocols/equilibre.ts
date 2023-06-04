4e70bad3fe372588c69c9d16431bed32221b0106
diego <admin@dbltecnologia.com.br>
2023-06-03 22:53:49 -0300

    Èquilibre emissions

--- protocols/equilibre.ts
+++ protocols/equilibre.ts
@@ -1,61 +1,64 @@
-import { Protocol } from "../types/adapters"; 
-import { manualCliff, manualLinear } from "../adapters/manual"; 
-import { periodToSeconds } from '../utils/time'; 
-import incentives from "../adapters/curve/community"; 
+import { LinearAdapterResult, Protocol } from "../types/adapters";
+import { manualCliff, manualLinear } from "../adapters/manual";
+import { periodToSeconds } from "../utils/time";
 
-const initialSupply = 40000000; 
-const weeklyDecayRate = 0.01; 
-const startTimestamp = 1677117405; 
-const oneWeekInSeconds = periodToSeconds.week; 
+const initialSupply = 40000000;
+const startTimestamp = 1677117405;
 
-const weeklyDecay = (supply: number, weeks: number): number => { 
-  for (let i = 0; i < weeks; i++) { 
-    supply -= supply * weeklyDecayRate; 
-  } 
-  return supply; 
-}; 
+const emissions = (portion: number): LinearAdapterResult[] => {
+  const totalWeeks = 52 * 5;
+  const weeklyDecayRate = 0.01;
+  let amount = initialSupply * 0.046;
+  let start = startTimestamp;
+  const sections: LinearAdapterResult[] = [];
+  for (let i = 0; i < totalWeeks; i++) {
+    sections.push({
+      type: "linear",
+      amount: amount * portion,
+      start,
+      end: start + periodToSeconds.week,
+    });
+    amount *= 1 - weeklyDecayRate;
+    start += periodToSeconds.week;
+  }
+  return sections;
+};
 
-function equilibre(): Protocol { 
-  const weeksSinceStart = (Date.now() / 1000 - startTimestamp) / oneWeekInSeconds; 
+const equilibre: Protocol = {
+  Emissions: emissions(0.94),
+  Community: manualCliff(startTimestamp, initialSupply * 0.1),
+  Team: [
+    manualCliff(startTimestamp, initialSupply * 0.15),
+    manualLinear(
+      startTimestamp,
+      startTimestamp + periodToSeconds.month * 3,
+      2100000,
+    ),
+    manualLinear(
+      startTimestamp,
+      startTimestamp + periodToSeconds.month * 6,
+      1200000,
+    ),
+    emissions(0.06),
+  ],
+  Grants: manualCliff(startTimestamp, initialSupply * 0.4),
+  Airdrop: manualCliff(startTimestamp, initialSupply * 0.01),
+  Liquidity: manualCliff(startTimestamp, initialSupply * 0.0025),
+  meta: {
+    sources: [
+      "https://equilibre-finance.gitbook.io/equilibre/emissions",
+      "https://equilibre-finance.gitbook.io/equilibre/initial-distribution",
+    ],
+    token: "vara:0xE1da44C0dA55B075aE8E2e4b6986AdC76Ac77d73",
+    protocolIds: ["566"],
+  },
+  sections: {
+    farming: ["Community"],
+    publicSale: ["Liquidity"],
+    airdrop: ["Airdrop"],
+    noncirculating: ["Grants"],
+    insiders: ["Team"],
+  },
+};
 
-  return { 
-    community: manualLinear( 
-      startTimestamp, 
-      startTimestamp + weeksSinceStart * oneWeekInSeconds, 
-      weeklyDecay(initialSupply * 0.1, weeksSinceStart),  // 10% for community fund
-    ), 
-    team: [ 
-      manualCliff(startTimestamp, initialSupply * 0.15), 
-      manualLinear( 
-        startTimestamp + 0.5 * periodToSeconds.year, 
-        startTimestamp + periodToSeconds.year, 
-        initialSupply * 0.15, 
-      ), 
-      manualLinear( 
-        startTimestamp + periodToSeconds.year, 
-        startTimestamp + 2 * periodToSeconds.year, 
-        initialSupply * 0.15, 
-      ), 
-    ], 
-    partnerProtocols: manualCliff(startTimestamp, initialSupply * 0.3375),  // 33.75% for partner protocols
-    grants: manualCliff(startTimestamp, initialSupply * 0.4),  // 40% for grants
-    airdrop: manualCliff(startTimestamp, initialSupply * 0.01),  // 1% for testnet airdrop
-    liquidity: manualCliff(startTimestamp, initialSupply * 0.0025),  // 0.25% for liquidity
-    meta: { 
-      sources: [
-        "https://equilibre-finance.gitbook.io/equilibre-v1/protocol-overview/tokenomics/emissions", 
-        "https://equilibre-finance.gitbook.io/equilibre-v1/protocol-overview/tokenomics/initial-distribution"
-      ], 
-      token: "vara:0xE1da44C0dA55B075aE8E2e4b6986AdC76Ac77d73", 
-      protocolIds: ["566"], 
-    }, 
-    sections: { 
-      farming: ["community", "liquidity"], 
-      airdrop: ["airdrop"], 
-      noncirculating: ["grants"], 
-      insiders: ["team"], 
-    }, 
-  }; 
-} 
-
-export default equilibre();
+export default equilibre;
