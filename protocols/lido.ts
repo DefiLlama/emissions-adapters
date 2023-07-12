913bc9683a57e97cbdc629df4406d4707da9cb9d
waynebruce0x <waynebruce0x@gmail.com>
2023-07-12 16:25:05 +0100

    lido

---
+++ protocols/lido.ts
@@ -0,0 +1,42 @@
+import { balance, latest } from "../adapters/balance";
+import { manualCliff, manualLinear, manualStep } from "../adapters/manual";
+import { Protocol } from "../types/adapters";
+import { periodToSeconds } from "../utils/time";
+
+const qty: number = 1e9;
+const token: string = "0x5a98fcbea516cf06857215779fd812ca3bef1b32";
+const start: number = 1554073200;
+const timestampDeployed: number = 1609804800;
+const chain: string = "ethereum";
+
+const schedule = (proportion: number) =>
+  manualLinear("2021-12-17", "2022-12-17", qty * proportion);
+
+const lido: Protocol = {
+  Investors: schedule(0.2218),
+  "Validators & Signature Holders": schedule(0.065),
+  "Initial Lido Devlopers": schedule(0.2),
+  "Founders & Future Employees": schedule(0.15),
+  "DAO Treasury": balance(
+    ["0x3e40D73EB977Dc6a537aF587D48316feE66E9C8c"],
+    token,
+    chain,
+    "lido-dao",
+    timestampDeployed,
+  ),
+  meta: {
+    notes: [],
+    sources: [`https://blog.lido.fi/introducing-ldo/`],
+    token: `${chain}:${token}`,
+    protocolIds: ["182"],
+    incompleteSections: [
+      {
+        key: "DAO Treasury",
+        allocation: qty * 0.3632,
+        lastRecord: () => latest("lido-dao", timestampDeployed),
+      },
+    ],
+  },
+  categories: {},
+};
+export default lido;
