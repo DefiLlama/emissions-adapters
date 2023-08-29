19954b0badb83387ba21c242b573a86c0314b89b
waynebruce0x <waynebruce0x@gmail.com>
2023-08-29 14:14:22 +0100

    convex

--- protocols/convex.ts
+++ protocols/convex.ts
@@ -37,7 +37,7 @@ const convex: Protocol = {
       {
         key: "Curve LP rewards",
         allocation: 50000000,
-        lastRecord: () => latest("convex-finance", deployTime),
+        lastRecord: () => 1621292400,
       },
     ],
   },
