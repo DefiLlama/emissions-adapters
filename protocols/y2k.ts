36d5f171422d5bb381d03ababb4adbdd2d459719
waynebruce0x <waynebruce0x@gmail.com>
2023-10-24 16:41:12 +0100

    y2k updates

--- protocols/y2k.ts
+++ protocols/y2k.ts
@@ -7,34 +7,27 @@ const qty: number = 20e6;
 const token: string = "0x5a98fcbea516cf06857215779fd812ca3bef1b32";
 const start: number = 1666911600;
 const chain: string = "arbitrum";
-
+const unlock1: number = 1696892400
+const unlock2: number = 1687129200
 const y2k: Protocol = {
   "Liquidity Mining": manualLinear(
     start,
     start + periodToSeconds.year * 4,
     qty * 0.3,
   ),
-  Treasury: manualLinear(start, start + periodToSeconds.year * 4, qty * 0.35),
+  Treasury: manualLinear(start, start + periodToSeconds.year * 4, qty * 0.425),
   "Core Team": [
-    manualCliff(start + periodToSeconds.month * 9, qty * 0.015),
-    manualLinear(
-      start + periodToSeconds.month * 9,
-      start + periodToSeconds.month * 24,
-      qty * 0.135,
-    ),
+    manualCliff(unlock1, qty * 0.015),
+    manualLinear(unlock1, unlock1 + periodToSeconds.month * 15, qty * 0.135),
   ],
   "New Order Treasury": manualLinear(
-    start,
-    start + periodToSeconds.year * 4,
-    qty * 0.1,
+    unlock1,
+    unlock1 + periodToSeconds.year * 4,
+    qty * 0.025,
   ),
   Investors: [
-    manualCliff(start + periodToSeconds.month * 6, qty * 0.005),
-    manualLinear(
-      start + periodToSeconds.month * 6,
-      start + periodToSeconds.month * 24,
-      qty * 0.045,
-    ),
+    manualCliff(unlock2, qty * 0.005),
+    manualLinear(unlock2, unlock2 + periodToSeconds.month * 18, qty * 0.045),
   ],
   IFO: manualCliff(start, qty * 0.05),
   meta: {
