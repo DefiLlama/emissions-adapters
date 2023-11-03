15a33adc388df126fce45aaf08b13f7d83583f44
realdealshaman <realdealshaman11@gmail.com>
2023-11-03 16:46:25 +0000

    update step and end date

--- protocols/x2y2.ts
+++ protocols/x2y2.ts
@@ -4,17 +4,18 @@ import { periodToSeconds } from "../utils/time";
 
 const totalSupply = 1_000_000_000;
 const start = 1645056000; 
+const end = 1707350400;
 
 const x2y2: Protocol = {
   "Staking Reward": manualStep(start, periodToSeconds.day, 1, 650_000_000), 
   "Liquidity Management": manualStep(start, periodToSeconds.day, 1, 15_000_000),
   "Airdrop": manualStep(start, periodToSeconds.day, 1, 120_000_000), 
   "Development & Team": [
-    manualCliff(start,25_000_000),
+    manualCliff(end - periodToSeconds.day,0),
     manualStep(start , periodToSeconds.day * 180, 4, 25_000_000), 
   ],
   "Treasury & Ecosystem": [
-    manualCliff(start,12_500_000),
+    manualCliff(end,12_500_000),
     manualStep(start  , periodToSeconds.day * 90, 8, 12_500_000), 
   ],
   "Presale": manualLinear(start, start + periodToSeconds.day * 360, 15_000_000), 
