93c3ef03e1ba9fa87de61ec88679a6989442c875
realdealshaman <realdealshaman11@gmail.com>
2023-12-03 05:58:27 +0000

    add tokenless protocols

--- no-emissions/protocols.ts
+++ no-emissions/protocols.ts
@@ -1,3 +1,45 @@
 export const protocolsWithNoEmissions:string[] = [
     "3031", // Metamask
+    "2258", // OpenSea Seaport
+    "2630", // OpenSea Seaport
+    "3377", // friend.tech
+    "3019", // Maestro
+    "3563", // cipher.rip
+    "3041", // Houdini Swap
+    "3336", // Banana Gun
+    "1892", // Azuro
+    "2414", // Blur Bids
+    "3872", // Bear Vs Bull
+    "3687", // Shuriken
+    "2959", // HoldStation DeFutures
+    "3265", // Agni Finance
+    "3499", // Ekubo
+    "3753", // XOXNO
+    "3747", // IntentX
+    "3846", // DLN
+    "3038", // Rainbow.me
+    "3835", // Substance Exchange
+    "3519", // BasePaint
+    "3585", // TangleSwap
+    "3238", // FusionX V2
+    "3239", // FusionX V3
+    "1468", // Pact
+    "2982", // Satori
+    "1241", // Mimo
+    "2804", // Forge
+    "3722", // Hipo
+    "2532", // Zonic
+    "3234", // DerpDEX
+    "3535", // PostTechSoFi
+    "3408", // Lynex
+    "3566", // Friend3
+    "3703", // Chainchat
+    "1053", // MooniSwap
+    "2610", // ZORA
+    "2734", // Blast API
+    "2513", // Liquid Bolt
+    "2889", // Rollup Finance vAMM
+    "3493", // FriendRoom
+    "3324", // Predy V5
+    "3246", // Sobal #200
 ]
