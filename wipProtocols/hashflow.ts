689f93c42f0c7331644de9f41dcdde6a871c0d05
realdealshaman <realdealshaman11@gmail.com>
2023-12-23 20:05:48 +0000

    update timestamp

--- wipProtocols/hashflow.ts
+++ wipProtocols/hashflow.ts
@@ -2,60 +2,60 @@ import { manualLinear, manualCliff } from "../adapters/manual";
 import { Protocol } from "../types/adapters";
 import { periodToSeconds } from "../utils/time";
 
-const HFT_TGE = 1649721600; // Hashflow Token Generation Event UNIX timestamp
+const start = 1667779200; // Nov 7, 2022
 const totalSupply = 1_000_000_000; // Total HFT Supply
 
 const hashflow: Protocol = {
     "Community Treasury": manualCliff(
-        HFT_TGE, 
+        start, 
         totalSupply * 0.01 // 1%
     ),
     "Future Hires": manualCliff(
-        HFT_TGE, 
+        start, 
         totalSupply * 0.025 // 2.5%
     ),
     "Designated Market Maker Loans": manualCliff(
-        HFT_TGE, 
+        start, 
         totalSupply * 0.075 // 7.5%
     ),
     "Early Integration Partners": manualLinear(
-        HFT_TGE, 
-        HFT_TGE + periodToSeconds.year, 
+        start, 
+        start + periodToSeconds.year, 
         totalSupply * 0.062 // 6.2%
     ),
     "Future Community Rewards": manualLinear(
-        HFT_TGE, 
-        HFT_TGE + periodToSeconds.year * 4, 
+        start, 
+        start + periodToSeconds.year * 4, 
         totalSupply * 0.0335 // 3.35%
     ),
     "Vendors and Early Service Providers": manualLinear(
-        HFT_TGE, 
-        HFT_TGE + periodToSeconds.year, 
+        start, 
+        start + periodToSeconds.year, 
         totalSupply * 0.0252 // 2.52%
     ),
     "Hashverse Rewards": manualLinear(
-        HFT_TGE, 
-        HFT_TGE + periodToSeconds.year * 4, 
+        start, 
+        start + periodToSeconds.year * 4, 
         totalSupply * 0.01 // 1%
     ),
     "Core Team": manualLinear(
-        HFT_TGE + periodToSeconds.year, 
-        HFT_TGE + periodToSeconds.year * 4, 
+        start + periodToSeconds.year, 
+        start + periodToSeconds.year * 4, 
         totalSupply * 0.1932 // 19.32%
     ),
     "Ecosystem Partners": manualLinear(
-        HFT_TGE + periodToSeconds.year, 
-        HFT_TGE + periodToSeconds.year * 3, 
+        start + periodToSeconds.year, 
+        start + periodToSeconds.year * 3, 
         totalSupply * 0.1854 // 18.54%
     ),
     "Community Rewards": manualLinear(
-        HFT_TGE + periodToSeconds.year, 
-        HFT_TGE + periodToSeconds.year * 3, 
+        start + periodToSeconds.year, 
+        start + periodToSeconds.year * 3, 
         totalSupply * 0.1308 // 13.08%
     ),
     "Early Investors": manualLinear(
-        HFT_TGE + periodToSeconds.year, 
-        HFT_TGE + periodToSeconds.year * 3, 
+        start + periodToSeconds.year, 
+        start + periodToSeconds.year * 3, 
         totalSupply * 0.25 // 25%
     ),
 
