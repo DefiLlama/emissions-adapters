24f1a5076b71e5419da5bd50ab7c0ce4bdca4167
waynebruce0x <waynebruce0x@gmail.com>
2024-03-26 16:13:13 +0000

    ens fix

--- protocols/ens.ts
+++ protocols/ens.ts
@@ -8,40 +8,40 @@ const qty = 100000000; //100m
 const token = "0xc18360217d8f7ab5e7c516566761ea12ce7f9d72";
 const chain = "ethereum";
 const ens: Protocol = {
-  "DAO Community Treasury": () =>
-    balance(
-      ["0xFe89cc7aBB2C4183683ab71653C4cdc9B02D44b7"],
-      token,
-      chain,
-      "ens",
-      1649113200,
-    ),
-  "Core Contributors": () =>
-    balance(
-      ["0x690F0581eCecCf8389c223170778cD9D029606F2"],
-      token,
-      chain,
-      "ens",
-      1649113200,
-    ),
+  // "DAO Community Treasury": () =>
+  //   balance(
+  //     ["0xFe89cc7aBB2C4183683ab71653C4cdc9B02D44b7"],
+  //     token,
+  //     chain,
+  //     "ens",
+  //     1649113200,
+  //   ),
+  // "Core Contributors": () =>
+  //   balance(
+  //     ["0x690F0581eCecCf8389c223170778cD9D029606F2"],
+  //     token,
+  //     chain,
+  //     "ens",
+  //     1649113200,
+  //   ),
   Airdrop: manualCliff(start, qty * 0.25),
   "Other contributors": manualLinear(
     start,
     start + 4 * periodToSeconds.year,
     0.0546 * qty,
   ),
-  documented: {
-    replaces: ["DAO Community Treasury", "Core Contributors"],
-    "Core Contributors": manualLinear(
-      start,
-      start + 4 * periodToSeconds.year,
-      0.1954 * qty,
-    ),
-    "DAO Community Treasury": [
-      manualCliff(start, 5000000), // 5m tokens release at start
-      manualStep(start, periodToSeconds.month, 48, 45000000 / 48), // monthly steps for the next 48 months
-    ],
-  },
+  // documented: {
+  // replaces: ["DAO Community Treasury", "Core Contributors"],
+  "Core Contributors": manualLinear(
+    start,
+    start + 4 * periodToSeconds.year,
+    0.1954 * qty,
+  ),
+  "DAO Community Treasury": [
+    manualCliff(start, 5000000), // 5m tokens release at start
+    manualStep(start, periodToSeconds.month, 48, 45000000 / 48), // monthly steps for the next 48 months
+  ],
+  // },
   meta: {
     notes: [
       `Tokens for core contributors and launch advisors will have a four year lock-up and vesting schedule.`,
@@ -52,18 +52,18 @@ const ens: Protocol = {
     ],
     protocolIds: ["2519"],
     total: qty,
-    incompleteSections: [
-      {
-        key: "Core Contributors",
-        allocation: qty * 0.1896,
-        lastRecord: () => latest("ens", 1649113200),
-      },
-      {
-        key: "DAO Community Treasury",
-        allocation: qty * 0.5,
-        lastRecord: () => latest("ens", 1649113200),
-      },
-    ],
+    // incompleteSections: [
+    //   {
+    //     key: "Core Contributors",
+    //     allocation: qty * 0.1896,
+    //     lastRecord: () => latest("ens", 1649113200),
+    //   },
+    //   {
+    //     key: "DAO Community Treasury",
+    //     allocation: qty * 0.5,
+    //     lastRecord: () => latest("ens", 1649113200),
+    //   },
+    // ],
   },
   categories: {
     insiders: [
