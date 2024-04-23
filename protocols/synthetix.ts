10415b2354f1c0a43453cf3ccaa9ca0c855b6ddd
waynebruce0x <waynebruce0x@gmail.com>
2024-04-23 15:28:06 +0100

    snx

---
+++ protocols/synthetix.ts
@@ -0,0 +1,31 @@
+import { supply, latest } from "../adapters/supply";
+import { Protocol } from "../types/adapters";
+
+const start = 1565395200;
+const token = "0xC011a73ee8576Fb46F5E1c5751cA3B9Fe0af2a6F";
+const chain = "ethereum";
+
+const synthetix: Protocol = {
+  Miscellaneous: () => supply(chain, token, start, "synthetix"),
+  meta: {
+    token: `${chain}:${token}`,
+    sources: [
+      "https://www.binance.com/en/research/projects/synthetix",
+      "https://docs.synthetix.io/synthetix-protocol/the-synthetix-protocol/synthetix-token-snx",
+    ],
+    notes: [
+      `There have been many changes to SNX tokenomicsa and it is difficult to describe the token allocations and vesting schedules.`,
+    ],
+    protocolIds: ["115"],
+    incompleteSections: [
+      {
+        key: "Miscellaneous",
+        lastRecord: () => latest("synthetix", start),
+        allocation: undefined,
+      },
+    ],
+  },
+  categories: {},
+};
+
+export default synthetix;
