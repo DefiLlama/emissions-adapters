8476f7e1f372f2bd41d7542530f49c0d0a9963bb
waynebruce0x <waynebruce0x@gmail.com>
2024-05-16 08:27:40 +0100

    jito

---
+++ protocols/jito.ts
@@ -0,0 +1,57 @@
+import { manualCliff, manualLinear } from "../adapters/manual";
+import { Protocol } from "../types/adapters";
+import { periodToSeconds } from "../utils/time";
+
+const start = 1701907200;
+const total = 1e9;
+
+const jito: Protocol = {
+  Investors: [
+    manualCliff(start + periodToSeconds.year, (total * 0.162) / 3),
+    manualLinear(
+      start + periodToSeconds.year,
+      start + periodToSeconds.years(2),
+      (total * 0.162 * 2) / 3,
+    ),
+  ],
+  "Core Contributors": [
+    manualCliff(start + periodToSeconds.year, (total * 0.245) / 3),
+    manualLinear(
+      start + periodToSeconds.year,
+      start + periodToSeconds.years(2),
+      (total * 0.245 * 2) / 3,
+    ),
+  ],
+  "Ecosystem Development": manualLinear(
+    start,
+    start + periodToSeconds.years(6),
+    total * 0.25,
+  ),
+  Airdrop: [
+    manualCliff(start, total * 0.09),
+    manualLinear(start, start + periodToSeconds.year, total * 0.01),
+  ],
+  "Community Growth": manualLinear(
+    start,
+    start + periodToSeconds.years(6),
+    total * 0.243,
+  ),
+  meta: {
+    token: `coingecko:jito-governance-token`,
+    sources: [
+      "https://www.jito.network/blog/announcing-jto-the-jito-governance-token/",
+    ],
+    notes: [
+      `From the chart given in the source material, Jito estimate ~6 year linear vesting schedule for Community Growth and Ecosystem Development sections. Therefore we have used the same in this analysis`,
+    ],
+    protocolIds: ["2308"],
+  },
+  categories: {
+    insiders: ["Investors", "Core Contributors"],
+    noncirculating: ["Ecosystem Development"],
+    airdrop: ["Airdrop"],
+    farming: ["Community Growth"],
+  },
+};
+
+export default jito;
