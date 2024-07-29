3f1c155316676e6055034716f1db2aaa1793a758
waynebruce0x <waynebruce0x@gmail.com>
2024-07-29 16:11:17 +0100

    worldcoin update

--- protocols/worldcoin.ts
+++ protocols/worldcoin.ts
@@ -1,8 +1,8 @@
-import { manualCliff, manualLinear, manualStep } from "../adapters/manual";
+import { manualCliff, manualLinear } from "../adapters/manual";
 import { Protocol } from "../types/adapters";
 import { periodToSeconds } from "../utils/time";
 
-const start = 1690153200;
+const start = 1690156800;
 const qty = 1e10;
 const token = "0x163f8c2467924be0ae7b5347228cabf260318753";
 const chain = "ethereum";
@@ -33,16 +33,30 @@ const worldcoin: Protocol = {
       qty * 0.0875,
     ),
   ],
-  Investors: manualLinear(
-    start + periodToSeconds.year,
-    start + periodToSeconds.year * 3,
-    qty * 0.135,
-  ),
-  "Initial Development Team": manualLinear(
-    start + periodToSeconds.year,
-    start + periodToSeconds.year * 3,
-    qty * 0.098,
-  ),
+  Investors: [
+    manualLinear(
+      start + periodToSeconds.year,
+      start + periodToSeconds.year * 3,
+      qty * 0.135 * 0.2,
+    ),
+    manualLinear(
+      start + periodToSeconds.year,
+      start + periodToSeconds.year * 5,
+      qty * 0.135 * 0.8,
+    ),
+  ],
+  "Initial Development Team": [
+    manualLinear(
+      start + periodToSeconds.year,
+      start + periodToSeconds.year * 3,
+      qty * 0.098 * 0.2,
+    ),
+    manualLinear(
+      start + periodToSeconds.year,
+      start + periodToSeconds.year * 5,
+      qty * 0.098 * 0.8,
+    ),
+  ],
   Reserve: manualLinear(
     start + periodToSeconds.year,
     start + periodToSeconds.year * 3,
@@ -51,9 +65,9 @@ const worldcoin: Protocol = {
   meta: {
     token: `${chain}:${token}`,
     sources: [
+      `https://www.toolsforhumanity.com/announcements/lockup-extension`,
       "https://whitepaper.worldcoin.org/tokenomics#circulating-supply-at-launch",
     ],
-    notes: [],
     protocolIds: ["4612"],
   },
   categories: {
