b6f0623be162e0d762df6e45307b9d24e55370aa
waynebruce0x <waynebruce0x@gmail.com>
2025-03-05 14:00:05 +0000

    hyperliquid patch

--- protocols/hyperliquid.ts
+++ protocols/hyperliquid.ts
@@ -6,33 +6,26 @@ const start = 1732838400;
 const total = 1e9;
 
 const hyperliquid: Protocol = {
-  "Community Rewards": manualLinear(
-    start,
-    start + periodToSeconds.years(3),
-    total * 0.38888,
-  ),
+  "Hyper Foundation Budget": manualCliff(start, total * 0.06),
+  "Community Grants": manualCliff(start, total * 0.003),
+  "HIP-2": manualCliff(start, total * 0.00012),
   "Genesis Distribution": manualCliff(start, total * 0.31),
   "Core Contributors": manualLinear(
     start + periodToSeconds.year,
     start + periodToSeconds.years(3),
     total * 0.238,
   ),
-
-  "Hyper Foundation Budget": manualLinear(
+  "Community Rewards": manualLinear(
     start,
-    start + periodToSeconds.years(3),
-    total * 0.06,
+    start + periodToSeconds.days(3581), // start + periodToSeconds.days(35808.5),
+    total * 0.03889, // total * 0.38888,
   ),
-  "Community Grants": manualLinear(
-    start,
-    start + periodToSeconds.years(3),
-    total * 0.003,
-  ),
-  "HIP-2": manualCliff(start, total * 0.00012),
   meta: {
     notes: [
+      "The Community Rewards schedule has been linearly extrapolated using the rate of unlocks as of 4 March 2025.",
+      "The remaining allocation, not shown on the chart, belongs to Community Rewards. It has been excluded here to avoid obscuring the remaining data.",
       "Most vesting schedules will complete between 2027–2028; some will continue after 2028. Here we have used an end date of end of 2027.",
-      "No information could be found regarding the unlock schedules of Community Rewards, Hyper Foundation Budget and Community Grants (44% of total). Her we have assumed a 3 year linear spend rate.",
+      "Although the full allocations for Hyper Foundation Budget and Community Grants were unlocked at TGE it is unclear what their spend rate is.",
     ],
     token: "coingecko:hyperliquid",
     sources: ["https://hyperfnd.medium.com/hype-genesis-1830a4dc2e3f"],
