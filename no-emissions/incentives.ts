71a340c0f0b0efc534fbd8566c6b4b741017cee4
Reynardo Etenia Wongso <reynardoew@gmail.com>
2025-05-23 22:11:19 +0700

    remove v1 from velodrome farming section, add incentives list

---
+++ no-emissions/incentives.ts
@@ -0,0 +1,12 @@
+export const protocolsIncentives:string[] = [
+    "2786", //bitcoin
+    "parent#velodrome",
+    "4611", //solana
+    "4488", //eth
+    "5978", //polkadot
+    "6016", //doge
+    "4499", //litecoin
+    "3562", //celestia
+    "6101", //tezos
+    "6103", //tron
+]
