5a6654f08fffcb4e57d40368d9de24c2363ef10b
Reynardo Etenia Wongso <reynardoew@gmail.com>
2025-05-27 22:49:50 +0700

    pendle, fix sky, test indexer

--- protocols/pendle.ts
+++ protocols/pendle.ts
@@ -1,31 +1,32 @@
 import { manualCliff, manualLinear, manualStep } from "../adapters/manual";
-import { Protocol, AdapterResult } from "../types/adapters";
-import { periodToSeconds } from "../utils/time";
+import { Protocol, AdapterResult, CliffAdapterResult } from "../types/adapters";
+import { queryAggregatedDailyLogsAmounts, queryAggregatedDailyLogsAmountsMulti } from "../utils/queries";
+import { periodToSeconds, readableToSeconds } from "../utils/time";
 
 const start: number = 1619478000;
 const qty: number = 251_061_124;
-const initialRate: number = 1_200_000;
-const decay: number = 0.01;
-const totalWeeks = 300;
 
-const incentives = () => {
-  const chartSections: AdapterResult[] = [];
-  let thisStart = start;
-  let amount = initialRate;
-  for (let i = 0; i < totalWeeks; i++) {
-    chartSections.push(
-      manualLinear(thisStart, thisStart + periodToSeconds.week, amount),
-    );
-    thisStart += periodToSeconds.week;
-    if (i < 26) continue;
-    amount *= 1 - decay;
-    if (i > 260) amount = 115384;
+const incentives = async (): Promise<CliffAdapterResult[]> => {
+  const result: CliffAdapterResult[] = [];
+
+  const data = await queryAggregatedDailyLogsAmountsMulti({
+    addresses: ["0x47d74516b33ed5d70dde7119a40839f6fcc24e57", "0x1e56299ebc8a1010cec26005d12e3e5c5cc2db00", "0x6875e4a945e498fe1b90bbb13cfbaf0b68658c9c", "0x704478dd72fd7f9b83d1f1e0fc18c14b54f034d0", "0xee708fc793a02f1edd5bb9dbd7fd13010d1f7136"],
+    topic0: "0xf7823f78d472190ac0f94e11854ed334dce4a2e571e5f1bf7a8aec9469891d97",
+    startDate: "2022-11-23",
+  })
+
+  for (let i = 0; i < data.length; i++) {
+    result.push({
+      type: "cliff",
+      start: readableToSeconds(data[i].date),
+      amount: Number(data[i].amount) / 1e18
+    });
   }
-  return chartSections;
-};
+  return result;
+}
 
 const pendle: Protocol = {
-  Incentives: incentives(),
+  Incentives: incentives,
   Investors: manualStep(start, periodToSeconds.month * 3, 4, (qty * 0.15) / 4),
   Advisors: manualStep(start, periodToSeconds.month * 3, 4, (qty * 0.01) / 4),
   "Liquidity bootstrapping": manualCliff(start, qty * 0.07),
@@ -39,19 +40,20 @@ const pendle: Protocol = {
       start + periodToSeconds.year,
       periodToSeconds.month * 3,
       4,
-      (qty * 0.11) / 4,
+      (qty * 0.11) / 4
     ),
   ],
   meta: {
     sources: ["https://medium.com/pendle/pendle-tokenomics-3a33d9caa0e4"],
     token: "ethereum:0x808507121b80c02388fad14726482e061b8da827",
     protocolIds: ["382"],
+    notes: ["Incentives are calculated from MarketClaimReward event. The data is fetched from the Ethereum, Arbitrum, Optimism, Sonic and BSC chains."]
   },
   categories: {
     farming: ["Incentives"],
     noncirculating: ["Ecosystem fund"],
     privateSale: ["Investors"],
-    insiders: ["Team","Advisors","Liquidity bootstrapping"],
+    insiders: ["Team", "Advisors", "Liquidity bootstrapping"],
   },
 };
 export default pendle;
--- utils/queries.ts
+++ utils/queries.ts
@@ -5,7 +5,7 @@ interface LogQueryParams {
   address: string;
   topic0: string;
   startDate: string;
-  endDate: string;
+  endDate?: string;
 }
 
 interface DailyAmount extends Row {
@@ -21,11 +21,34 @@ export async function queryAggregatedDailyLogsAmounts(params: LogQueryParams): P
     FROM evm_indexer.logs
     WHERE (address = {address:String}) 
       AND (topic0 = {topic0:String}) 
-      AND (timestamp >= toDateTime({startDate:String})) 
-      AND (timestamp < toDateTime({endDate:String}))
+      AND (timestamp >= toDateTime({startDate:String}))
+      ${params.endDate ? 'AND (timestamp < toDateTime({endDate:String}))' : ''}
     GROUP BY date
     ORDER BY date ASC
   `;
 
   return queryClickhouse<DailyAmount>(sql, params);
 } 
+
+export async function queryAggregatedDailyLogsAmountsMulti(params: {
+  addresses: string[];
+  topic0: string;
+  startDate: string;
+  endDate?: string;
+}): Promise<DailyAmount[]> {
+  const sql = `
+    SELECT
+      address,
+      toStartOfDay(timestamp) AS date,
+      SUM(reinterpretAsUInt256(reverse(unhex(substring(data, 3))))) AS amount
+    FROM evm_indexer.logs
+    WHERE (address IN ({addresses:Array(String)})) 
+      AND (topic0 = {topic0:String}) 
+      AND (timestamp >= toDateTime({startDate:String}))
+      ${params.endDate ? 'AND (timestamp < toDateTime({endDate:String}))' : ''}
+    GROUP BY address, date
+    ORDER BY address, date
+  `;
+
+  return queryClickhouse<DailyAmount>(sql, params);
+}
