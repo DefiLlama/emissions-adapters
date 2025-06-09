eb0f46d7698b888a7fc0402bbb81c5ef3452fae8
reynardoew <reynardoew@gmail.com>
2025-06-09 21:53:40 +0700

    radiant

--- protocols/radiant.ts
+++ protocols/radiant.ts
@@ -1,6 +1,7 @@
 import { manualCliff, manualLinear } from "../adapters/manual";
-import { Protocol } from "../types/adapters";
-import { periodToSeconds } from "../utils/time";
+import { CliffAdapterResult, Protocol } from "../types/adapters";
+import { queryCustom } from "../utils/queries";
+import { periodToSeconds, readableToSeconds } from "../utils/time";
 
 const start = 1658685600;
 const qty = 1000000000;
@@ -26,6 +27,40 @@ function yieldSchedule() {
   );
 }
 
+const rewards = async (): Promise<CliffAdapterResult[]> => {
+  const result: CliffAdapterResult[] = [];
+  const data = await queryCustom(`
+    SELECT
+      toStartOfDay(timestamp) AS date,
+      SUM(reinterpretAsUInt256(reverse(unhex(SUBSTRING(data, 3, 64))))) / 1e18 AS amount
+    FROM evm_indexer.logs
+    WHERE (
+      (topic0 = '0x540798df468d7b23d11f156fdb954cb19ad414d150722a7b6d55ba369dea792e'
+        AND address IN ('0xc2054a8c33bfce28de8af4af548c48915c455c13')
+        AND topic2 = '0x0000000000000000000000000c4681e6c0235179ec3d4f4fc4df3d14fdd96017')
+      OR
+      (topic0 = '0xa236f2dcd2b940fd86168787a5f820805cdbd85131f7192d9d9c418556876fca'
+        AND address IN (
+          '0x76ba3ec5f5adbf1c58c91e86502232317eea72de',
+          '0x4fd9f7c5ca0829a656561486bada018505dfcb5e'
+        )
+      )
+    )
+    GROUP BY date
+    ORDER BY date DESC;
+  `, {});
+
+  for (let i = 0; i < data.length; i++) {
+    result.push({
+      type: "cliff",
+      start: readableToSeconds(data[i].date),
+      amount: Number(data[i].amount),
+    });
+  }
+  return result;
+};
+
+
 const radiant: Protocol = {
   treasury: manualCliff(start, qty * 0.03),
   "dao reserve": manualCliff(start, qty * 0.14),
@@ -34,8 +69,8 @@ const radiant: Protocol = {
     start + periodToSeconds.year * 1.5,
     qty * 0.07,
   ),
-  "pool2 incentives": manualLinear("2022/08/03", "2023/03/17", qty * 0.02),
-  "supply and borrowers incentives": yieldSchedule(),
+  // "pool2 incentives": manualLinear("2022/08/03", "2023/03/17", qty * 0.02),
+  // "supply and borrowers incentives": yieldSchedule(),
   team: [
     manualLinear(
       start + periodToSeconds.month * 3,
@@ -44,16 +79,24 @@ const radiant: Protocol = {
     ),
     manualCliff(start + periodToSeconds.month * 3, qty * 0.2 * 0.1),
   ],
+  "Rewards": yieldFarming.map((amount, month) =>
+    manualLinear(
+      start + month * periodToSeconds.month,
+      start + (month + 1) * periodToSeconds.month,
+      amount,
+    ),
+  ),
   meta: {
     token: "arbitrum:0x0c4681e6c0235179ec3d4f4fc4df3d14fdd96017",
     sources: [
       "https://docs.radiant.capital/radiant/project-info/rdnt-tokenomics",
     ],
-    protocolIds: ["1922"],
+    protocolIds: ["parent#radiant"],
   },
   categories: {
     noncirculating: ["treasury", "dao reserve"],
-    farming: ["pool2 incentives", "supply and borrowers incentives"],
+    // farming: ["pool2 incentives", "supply and borrowers incentives"],
+    farming: ["Rewards"],
     insiders: ["core contributors and advisors", "team"],
   },
 };
