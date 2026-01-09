189a03772284f51b56a7215de9627ba616c8d63d
reynardoew <reynardoew@gmail.com>
2026-01-09 15:05:40 +0700

    update kamino

--- protocols/kamino.ts
+++ protocols/kamino.ts
@@ -3,26 +3,22 @@ import { manualCliff, manualLinear } from "../adapters/manual";
 import { periodToSeconds } from "../utils/time";
 
 const total = 1e10;
-const start = 1714435200; // 30 Apr
+const start = 1714435200;
 const token = "KMNo3nJsBXfcpJTVhZcXLW7RmTwTt4GVFE7suUBo9sS";
 const chain = "solana";
 
 const kamino: Protocol = {
-  "Community & Grants": manualCliff(start, total * 0.065),
-  "Genesis Community Allocation": manualCliff(start, total * 0.075),
-  "Season 2 Airdrop": manualCliff(
-    1711411200 + periodToSeconds.days(90),
-    total * 0.035,
-  ),
-  "Liquidity & Treasury": manualLinear(
-    start,
-    start + periodToSeconds.days(11 * 128),
-    total * 0.275,
-  ), // 250m over 128 days, 27.5% = 11 * 128 days
+  "Community & Grants": manualCliff(start, total * 0.185),
+  "Liquidity & Treasury": manualCliff(start, total * 0.10),
+  "Genesis Allocation": manualCliff(start, total * 0.075),
+  "Season 2 Allocation": manualCliff(1724630400, total * 0.035),
+  "Season 3 Allocation": manualCliff(1748822400, total * 0.035),
+  "Season 4 Allocation": manualCliff(1763337600, total * 0.01),
+  "Season 5 Allocation": manualCliff(1770595200, total * 0.01),
   "Core Contributors": manualLinear(
     start + periodToSeconds.year,
     start + periodToSeconds.years(3),
-    total * 0.2,
+    total * 0.20,
   ),
   "Key Stakeholders & Advisors": manualLinear(
     start + periodToSeconds.year,
@@ -30,18 +26,16 @@ const kamino: Protocol = {
     total * 0.35,
   ),
   meta: {
-    notes: [
-      `Between 30 Apr and 4 Sep '24, 250m KMNO were spent from the Liquidity & Treasury allocation. In this analysis we have extrapolated this rate of spending.`,
-    ],
     sources: ["https://docs.kamino.finance/kmno/token-info"],
     token: `${chain}:${token}`,
     protocolIds: ["2062", "3770"],
   },
   categories: {
-    publicSale: ["Liquidity & Treasury"],
-    insiders: ["Core Contributors", "Key Stakeholders & Advisors"],
-    airdrop: ["Genesis Community Allocation", "Season 2 Airdrop"],
     noncirculating: ["Community & Grants"],
+    liquidity: ["Liquidity & Treasury"],
+    airdrop: ["Genesis Allocation", "Season 2 Allocation", "Season 3 Allocation", "Season 4 Allocation", "Season 5 Allocation"],
+    insiders: ["Core Contributors", "Key Stakeholders & Advisors"],
   },
 };
+
 export default kamino;
