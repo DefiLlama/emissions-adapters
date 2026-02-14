209e29ec7df0f60a6720b79a41b3d449de8bda96
Felix Bruguera <felixbruguera0@gmail.com>
2026-02-14 09:14:11 -0400

    track lighter unlocks

---
+++ protocols/lighter.ts
@@ -0,0 +1,34 @@
+import { manualCliff, manualLinear } from "../adapters/manual";
+import { Protocol } from "../types/adapters";
+import { periodToSeconds } from "../utils/time";
+
+const start = 1766966400; // 2025-12-29
+const total = 1_000_000_000;
+const shares = {
+    airdrop: total * 0.25,
+    ecosystem: total * 0.25,
+    team: total * 0.26,
+    investors: total * 0.24
+}
+
+const lighter: Protocol = {
+  "Seasons 1 and 2 Airdrop": manualCliff(start, shares.airdrop),
+  "Ecosystem allocation": manualCliff(start, shares.ecosystem),
+  "Team": manualLinear(start + periodToSeconds.year, start + periodToSeconds.years(4), shares.team),
+  "Investors": manualLinear(start + periodToSeconds.year, start + periodToSeconds.years(4), shares.investors),
+  meta: {
+    notes: [
+      "The remaining ecosystem allocation after the airdrop will be used for future points seasons, partnerships and growth initiatives.",
+    ],
+    token: "coingecko:lighter",
+    sources: ["https://x.com/Lighter_xyz/status/2005862682608472263"],
+    protocolIds: ["parent#lighter"],
+    total,
+  },
+  categories: {
+    insiders: ["Team", "Investors"],
+    noncirculating: ["Ecosystem allocation"],
+    airdrop: ["Seasons 1 and 2 Airdrop"],
+  },
+};
+export default lighter;
