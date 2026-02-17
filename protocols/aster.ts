c1903ccc5fa370a244fe15a9a74eaf95addd476d
Felix Bruguera <felixbruguera0@gmail.com>
2026-02-17 11:30:38 -0400

    track aster unlocks

---
+++ protocols/aster.ts
@@ -0,0 +1,40 @@
+import { manualCliff, manualLinear } from "../adapters/manual";
+import { Protocol } from "../types/adapters";
+import { periodToSeconds } from "../utils/time";
+
+const start = 1758067200; // 2025-09-17
+const total = 8_000_000_000;
+const shares = {
+    initialAirdrop: total * 0.088,
+    remainingAirdrops: total * 0.447,
+    ecosystem: total * 0.30,
+    treasury: total * 0.07,
+    team: total * 0.05,
+    liquidity: total * 0.045
+}
+
+const lighter: Protocol = {
+    "Ecosystem & Community": manualLinear(start, start + periodToSeconds.months(20), shares.ecosystem),
+    "Team": manualLinear(start + periodToSeconds.year, start + periodToSeconds.months(40), shares.team),
+    "Treasury": manualCliff(start, shares.treasury),
+    "Airdrop": [manualCliff(start, shares.initialAirdrop), manualLinear(start, start + periodToSeconds.months(80), shares.remainingAirdrops)],
+    "Initial Liquidity": manualCliff(start, shares.liquidity),
+    meta: {
+        notes: [
+        "8.8% of the total supply was airdropped at TGE, the remaining airdrop allocation will be gradually released over the next 80 months.",
+        "The treasury allocation will not enter circulating supply after TGE and remain fully locked until utilized via governance-approved mechanisms"
+        ],
+        token: "coingecko:aster-2",
+        sources: ["https://docs.asterdex.com/usdaster/tokenomics"],
+        protocolIds: ["parent#astherus"],
+        total,
+    },
+    categories: {
+        insiders: ["Team"],
+        farming: ["Ecosystem & Community"],
+        publicSale: ["Initial Liquidity"],
+        noncirculating: ["Treasury"],
+        airdrop: ["Airdrop"],
+    },
+};
+export default lighter;
