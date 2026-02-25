d4f7ee2da94bc0dca1ae95115682836c0e7b4520
Felix Bruguera <felixbruguera0@gmail.com>
2026-02-25 19:56:46 -0400

    track solver rewards and remove old adapter

--- protocols/cowSwap.ts
+++ protocols/cowSwap.ts
@@ -1,11 +1,11 @@
 import { manualCliff, manualLinear } from "../adapters/manual";
 import { Protocol } from "../types/adapters";
-import { years } from "../utils/time";
+import { queryDailyOutflows } from "../utils/queries";
+import { readableToSeconds, years } from "../utils/time";
 
 const start = 1648425600; // 2022-03-28
 const total = 1_000_000_000;
 const shares = {
-    treasury: total * 0.444,
     team: total * 0.15,
     gnosisDAO: total * 0.10,
     airdrop: total * 0.10,
@@ -13,15 +13,30 @@ const shares = {
     advisory: total * 0.006,
     investmentRound: total * 0.10,
 }
+const solverRewards = "0xA03be496e67Ec29bC62F01a428683D7F9c204930"
+const token = "0xdef1ca1fb7fbcdc777520aa7f396b4e015f497ab"
+
+async function getSolverRewards() {
+    const data = await queryDailyOutflows({
+      token: token,
+      fromAddress: solverRewards,
+      startDate: "2022-03-28"
+    })
+    return data.map(d => ({
+      type: "cliff" as const,
+      start: readableToSeconds(d.date),
+      amount: Number(d.amount),
+    }))
+}
 
 const cowSwap: Protocol = {
-    "CoW DAO Treasury": manualCliff(start, shares.treasury),
     "CoWmunity Airdrop": manualCliff(start, shares.airdrop),
     "Team": manualLinear(start, years(start, 4), shares.team),
     "GnosisDAO": manualLinear(start, years(start, 4), shares.gnosisDAO),
     "CoWmunity Investment": manualLinear(start, years(start, 4), shares.communityInvestment),
     "Investment Round": manualLinear(start, years(start, 4), shares.investmentRound),
     "CoW Advisory": manualLinear(start, years(start, 4), shares.advisory),
+    "Solver Rewards": getSolverRewards,
     meta: {
         notes: [
             "vCOW tokens vest linearly over 4 years from deployment for team, advisors, investors, and GnosisDAO.",
@@ -37,7 +52,7 @@ const cowSwap: Protocol = {
         privateSale: ["Investment Round"],
         airdrop: ["CoWmunity Airdrop"],
         publicSale: ["CoWmunity Investment"],
-        noncirculating: ["CoW DAO Treasury"],
+        farming: ["Solver Rewards"],
     },
 };
 export default cowSwap;
