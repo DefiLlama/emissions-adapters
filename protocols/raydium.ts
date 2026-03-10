dff251d592886e8be3e3f086e912b989e78712d1
Felix Bruguera <felixbruguera0@gmail.com>
2026-03-10 10:19:16 -0400

    track raydium incentives with dune and update source

--- protocols/raydium.ts
+++ protocols/raydium.ts
@@ -1,6 +1,7 @@
-import { Protocol } from "../types/adapters";
+import { CliffAdapterResult, Protocol } from "../types/adapters";
 import { manualLinear, manualCliff } from "../adapters/manual";
 import { periodToSeconds } from "../utils/time";
+import { queryDune } from "../utils/dune";
 
 const totalSupply = 555_000_000; 
 const RAY_TGE = 1613865600; // Sunday, February 21, 2021
@@ -13,22 +14,13 @@ const liquidityAllocation = totalSupply * 0.08; // 8%
 const communitySeedAllocation = totalSupply * 0.06; // 6%
 const advisorsAllocation = totalSupply * 0.02; // 2%
 
-// Custom function for Mining Reserve with halving
-function miningReserveVesting() {
-    let vestingSchedules = [];
-    let remainingAllocation = miningReserveTotalAllocation;
-    let currentStart = RAY_TGE;
-    const halvingPeriod = periodToSeconds.month * 6;
-
-    for (let i = 0; i < 6; i++) { // 6 periods of 6 months each
-        let periodAllocation = remainingAllocation / (6 - i); // Dividing remaining allocation equally among remaining periods
-        vestingSchedules.push(manualLinear(currentStart, currentStart + halvingPeriod, periodAllocation));
-
-        currentStart += halvingPeriod;
-        remainingAllocation -= periodAllocation;
-    }
-
-    return vestingSchedules;
+const miningReserve = async (): Promise<CliffAdapterResult[]> => {
+    const data = await queryDune('6804761', true)
+    return data.map((row: Record<string, string>) => ({
+        type: 'cliff',
+        start: row.date,
+        amount: Number(row.amount),
+    }))
 }
 
 const raydium: Protocol = {
@@ -36,7 +28,7 @@ const raydium: Protocol = {
         RAY_TGE, 
         liquidityAllocation
     ),
-    "Mining Reserve": miningReserveVesting(),
+    "Mining Reserve": miningReserve,
     "Partnership & Ecosystem": manualLinear(
         RAY_TGE, 
         RAY_TGE + periodToSeconds.year * 3, 
@@ -60,11 +52,11 @@ const raydium: Protocol = {
     meta: {
         notes: [
             `The emission and distribution details are based on the proposed and intended token emission and distribution for RAY tokens.`,
-            `Emissions have been slower than originally planned and mining reserve will likely continue to be emitted longer than the initial 36 months, it's vesting at a rate of 134,649.95 RAY per day; at current rates, there's roughly 40 years left of liquidity mining`,
+            `Mining Reserve has a 34% allocation and its circulating supply is calculated by tracking the outflows from the address: fArUAncZwVbMimiWv5cPUfFsapdLd8QMXZE4VXqFagR`,
           ],
         token: "solana:4k3Dyjzvzp8eMZWUXbBCjEvwSkkk59S5iCNLY3QrkX6R",
         sources: [
-            "https://docs.raydium.io/raydium/ray_token/the-ray-token"
+            "https://docs.raydium.io/raydium/protocol"
         ],
         protocolIds: ["214"],
     },
