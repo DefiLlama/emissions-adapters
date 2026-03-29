8f309f5c6d697bf416a9d6acae0608b0ae93404e
Felix Bruguera <felixbruguera0@gmail.com>
2026-03-29 15:17:33 -0400

    fix dodo

--- protocols/dodo.ts
+++ protocols/dodo.ts
@@ -1,61 +1,45 @@
 import { Protocol } from "../types/adapters";
 import { manualCliff, manualLinear } from "../adapters/manual";
-import { periodToSeconds } from "../utils/time";
-import { balance, latest } from "../adapters/balance";
+import { years, months } from "../utils/time";
 
 const qty = 1000000000;
 const start = 1597449600;
 const token = "0x43dfc4159d86f3a37a5a4b3d4580b888ad7d4ddd";
 const chain = "ethereum";
-const timestampDeployed = 1602457200;
 
 const dodo: Protocol = {
-  "Team & consultants": manualLinear(
-    start + periodToSeconds.year,
-    start + periodToSeconds.year * 3,
+  "Community Incentives": manualCliff(start, qty * 0.6),
+  "Team & Consultants": manualLinear(
+    years(start, 1),
+    years(start, 3),
     qty * 0.15,
   ),
-  "Seed investors": manualLinear(
-    start + periodToSeconds.year,
-    start + periodToSeconds.year * 3,
+  "Seed Investors": manualLinear(
+    years(start, 1),
+    years(start, 3),
     qty * 0.06,
   ),
-  "private round investors": [
-    manualCliff(start + periodToSeconds.month * 6, qty * 0.1 * 0.1),
+  "Private Round Investors": [
+    manualCliff(months(start, 6), qty * 0.1 * 0.1),
     manualLinear(
-      start + periodToSeconds.month * 6,
-      start + periodToSeconds.month * 18,
+      months(start, 6),
+      months(start, 18),
       qty * 0.1 * 0.9,
     ),
   ],
   IDO: manualCliff(start, qty * 0.01),
-  "Operations, marketing, partners": manualCliff(start, qty * 0.08),
-  "Community incentives": (backfill: boolean) =>
-    balance(
-      ["0x4447183c50e82a8b0141718c405381a3b1bad634"],
-      token,
-      chain,
-      "dodo",
-      timestampDeployed,
-      backfill,
-    ),
+  "Operations, Marketing & Partners": manualCliff(start, qty * 0.08),
   meta: {
-    sources: ["https://docs.dodoex.io/english/tokenomics/dodo-allocation"],
+    sources: [
+      "https://docs.dodoex.io/en/home/token-economy"
+  ],
     token: `${chain}:${token}`,
-    protocolIds: ["146"],
+    protocolIds: ["parent#dodo"],
     total: qty,
-    incompleteSections: [
-      {
-        key: "Community incentives",
-        allocation: qty * 0.6,
-        lastRecord: (backfill: boolean) =>
-          latest("dodo", timestampDeployed, backfill),
-      },
-    ],
   },
   categories: {
     publicSale: ["IDO"],
-    noncirculating: ["Community incentives"],
+    farming: ["Community incentives"],
     privateSale: ["Seed investors", "private round investors"],
     insiders: ["Team & consultants", "Operations, marketing, partners"],
   },
