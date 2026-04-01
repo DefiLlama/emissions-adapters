74c796af9b017c732501069e3279faa7dea4eb61
FelixBruguera <felixbruguera0@gmail.com>
2026-04-01 11:14:26 -0400

    add incentivesOnly and use dune cache in ore (#272)

--- protocols/ore.ts
+++ protocols/ore.ts
@@ -1,15 +1,19 @@
 import { CliffAdapterResult, Protocol } from "../types/adapters";
-import { queryDune } from "../utils/dune";
+import { queryDune, queryDuneSQLCached } from "../utils/dune";
 import { stringToTimestamp } from "../utils/time";
 
 const duneData = async (): Promise<CliffAdapterResult[]> => {
-    const data = await queryDune('6621804', true)
-    return data.map((row: Record<string, string>) => ({
-        type: 'cliff',
-        start: row.date,
-        amount: Number(row.amount),
-        isUnlock: false
-    }))
+    return queryDuneSQLCached(`
+        SELECT
+            to_unixtime(call_block_date) as date,
+            SUM(amount) / 1e11 AS amount
+        FROM spl_token_solana.spl_token_call_mintTo
+        WHERE account_mint = 'oreoU2P8bN6jkk3jbaiVxYnG1dCXcYxwhwyK9jSybcp'
+            AND account_account = 'GwZS8yBuPPkPgY4uh7eEhHN5EEdpkf7EBZ1za6nuP3wF'
+            AND call_block_time >= START
+        GROUP BY call_block_date
+        ORDER BY call_block_date ASC
+`, 1755043200, { protocolSlug: "ore-protocol", allocation: "Mining"})
 }
 const ore : Protocol = {
     "Mining": duneData,
@@ -18,7 +22,8 @@ const ore : Protocol = {
         sources: [
             "https://ore.supply/about#mining"
         ],
-        protocolIds: ["6893"]
+        protocolIds: ["6893"],
+        incentivesOnly: true,
     },
     categories: {
         farming: ["Mining"],
